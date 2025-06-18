7a3120f39bec2c1045613616253dd242dc28dc68
Ben Getches <benjamin.getches@colorado.edu>
2025-06-17 18:59:18 -0600

    added port 3000

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # from frontend URL
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # from frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
